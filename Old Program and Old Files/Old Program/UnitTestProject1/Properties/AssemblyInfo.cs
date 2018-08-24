a938935c0a2eed912b40faae12b70b3d1602637b
Aaron <taylora@deakin.edu.au>
2018-08-24 21:12:34 +1000

    q

--- Old Program and Old Files/Old Program/UnitTestProject1/Properties/AssemblyInfo.cs
+++ Old Program and Old Files/Old Program/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("UnitTest1_1")]
+[assembly: AssemblyTitle("UnitTestProject1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnitTest1_1")]
+[assembly: AssemblyProduct("UnitTestProject1")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("e0e635f0-150a-4455-803a-fa108f49408b")]
+[assembly: Guid("5a6e170b-b73a-44a5-8a7e-d5e205330665")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
