f0a616f003e0ae13590f9a0b4a331ef96d1c1ab9
Aaron <taylora@deakin.edu.au>
2018-08-24 16:47:20 +1000

    public class Validator

---
+++ Old Program and Old Files/Old Program/UnitTest1.1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest1.1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest1.1")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aede4a28-de06-4186-a084-88e8ba4c0fc7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Old Program and Old Files/Old Program/UnitTest1.1/UnitTest1_1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest1_1
+{
+    [TestClass]
+    public class UnitTest1_1
+    {
+        public object Validator { get; private set; }
+
+        [TestMethod]
+        public void TestMethod1_1()
+        {
+            // Arrange
+            String hexBlack = "#000000";
+
+            Boolean expectedReturnBlack = false;
+
+            Boolean actualReturnBlack = Validator.IsHexColourCode(hexBlack);
+
+            Assert.AreEqual(expectedReturnBlack, actualReturnBlack, "failed ...");
+
+        }
+    }
+}
