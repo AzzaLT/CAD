53258b1187d924baf7d3d80e54894303cf8daaa9
Aaron <taylora@deakin.edu.au>
2018-08-24 21:59:02 +1000

    Actually got test working

--- Old Program and Old Files/Old Program/UnitTest1_1/Properties/AssemblyInfo.cs
+++ Old Program and Old Files/Old Program/UnitTest1_1/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyTitle("UnitTest1_1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyProduct("UnitTest1_1")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("5a6e170b-b73a-44a5-8a7e-d5e205330665")]
+[assembly: Guid("e1d441b2-bbe9-4568-86eb-6da4636ea23a")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
---
+++ Old Program and Old Files/Old Program/UnitTest1_1/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CrozzleApplication;
+namespace UnitTest1_1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            // Arrange.
+            String hexBlack = "#000000";
+            String hexRedish = "#AA0000";
+            String hexWhite = "#FFFFFF";
+            String invalid1 = "000000";
+            String invalid2 = "#00000X";
+            String invalid3 = "#!@#$%^";
+            Boolean expectedReturnBlack = true;
+            Boolean expectedReturnRedish = true;
+            Boolean expectedReturnWhite = true;
+            Boolean expectedReturnInvalid1 = false;
+            Boolean expectedReturnInvalid2 = false;
+            Boolean expectedReturnInvalid3 = false;
+            // Act.
+            Boolean actualReturnBlack = Validator.IsHexColourCode(hexBlack);
+            Boolean actualReturnRedish = Validator.IsHexColourCode(hexRedish);
+            Boolean actualReturnWhite = Validator.IsHexColourCode(hexWhite);
+            Boolean actualReturnInvalid1 = Validator.IsHexColourCode(invalid1);
+            Boolean actualReturnInvalid2 = Validator.IsHexColourCode(invalid2);
+            Boolean actualReturnInvalid3 = Validator.IsHexColourCode(invalid3);
+            // Assert.
+            Assert.AreEqual(expectedReturnBlack, actualReturnBlack, "failed ...");
+            Assert.AreEqual(expectedReturnRedish, actualReturnRedish, "failed ...");
+            Assert.AreEqual(expectedReturnWhite, actualReturnWhite, "failed ...");
+            Assert.AreEqual(expectedReturnInvalid1, actualReturnInvalid1, "failed ...");
+            Assert.AreEqual(expectedReturnInvalid2, actualReturnInvalid2, "failed ...");
+            Assert.AreEqual(expectedReturnInvalid3, actualReturnInvalid3, "failed ...");
+
+
+        }
+    }
+}
